794ad8d95585c20ae4436b83c3e68733dada82de
PHLP_Neo <phlp.neo@gmail.com>
2025-09-12 00:30:06 +1000

    Added exercises to Methods Added exercise "Starsign"

---
+++ part02-repetition/section04-Methods/part02-Part02_33.StarSign/src/main/java/StarSign.java
@@ -0,0 +1,58 @@
+
+public class StarSign {
+
+    public static void main(String[] args) {
+
+        
+        //The tests are not checking the main, so you can modify it freely.
+        //NB: If the tests don't seem to pass, you should try the methods here
+        //in the main to make sure they print the correct shapes!
+        
+        printStars(3);
+        System.out.println("\n---");  // printing --- between the shapes
+        printSquare(4);
+        System.out.println("\n---");
+        printRectangle(5, 6);
+        System.out.println("\n---");
+        printTriangle(3);
+        System.out.println("\n---");
+    }
+
+    public static void printStars(int number) {
+        // first part of the exercise
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+    }
+
+    public static void printSquare(int size) {
+        // second part of the exercise
+        for (int i = 0; i < size; i++) {
+            printStars(size);
+            //if (i+1<size) {
+            //    System.out.print("\n");
+            //}
+        }
+    }
+
+    public static void printRectangle(int width, int height) {
+        for (int i = 0; i < height; i++) {
+            printStars(width);
+            //if (i+1<height) {
+            //   System.out.print("\n");
+            //}
+        }
+        // third part of the exercise
+    }
+
+    public static void printTriangle(int size) {
+        // fourth part of the exercise
+        for (int i = 1; i <= size; i++) {
+            printStars(i);
+            //if (i<size) {
+            //    System.out.print("\n");
+            //}
+        }
+    }
+}
