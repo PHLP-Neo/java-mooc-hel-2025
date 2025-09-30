1db7b019f528752d2b2dc66112c762fe1f341d36
PHLP_Neo <phlp.neo@gmail.com>
2025-09-30 15:29:01 +1000

    Created new directory "OOP" Creted new section "introduction" Added exercise to "introduction" Added exercise 1

---
+++ part04-OOP/section01-Introduction/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,11 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account person1 = new Account("Person1", 100);
+        person1.deposit(20);
+        System.out.println(person1);
+    }
+}
